105d32bf6dcac1779d328f3eeed7a01194b1bbdc
Madison Turner <hmadisonturner@gmail.com>
2021-02-02 23:53:42 -0600

    Add characters query

--- src/Components/Page.js
+++ src/Components/Page.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { useQuery,
          gql
        } from '@apollo/client';
+import Card from './Card';
 
   const GET_STUFF = gql`
     query {
@@ -25,13 +26,44 @@ import { useQuery,
 //}).then(result => console.log(result))
 
 export default function Page(props) {
-  const { data } = useQuery(GET_STUFF);
+  let page = 1;
+
+  const GET_MORE = gql`
+    query {
+      characters ( page: ${page} ) {
+        info {
+          next
+        }
+        results {
+          id
+          name
+          status
+          species
+          type
+          origin {
+            name
+          }
+          location {
+            name
+          }
+          episode {
+            name
+            episode
+          }
+        }
+      }
+    }
+  `;
+
+
+
+  const { data } = useQuery(GET_MORE);
   return (
     <div>
-      <h1>{props.type}</h1>
+      <h1>{props.type.toUpperCase()}</h1>
     {data && (
       <>
-        {data.characters.results.map((character) => <div key={character.id} >{character.name}</div>)}
+        {data.characters.results.map((character) => <Card character={ character } />)}
       </>
     )}
     </div> 
